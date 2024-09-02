d91e38a51e0ce323c1e83dfe2a6630ac583c7e3f
Jari Nystedt <jari.nystedt@tietoevry.com>
2024-09-02 13:49:45 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
-
-import { HexToUuidInput } from './HexToUuidInput';
+import { HexToUuidInput, hexToUuid, uuidToHex } from './HexToUuidInput';
 import { HexToUuidConverter } from './HexToUuidInput';
+import { v4 as uuidv4 } from 'uuid'; // Make sure to install uuid package
+
 function App() {
   const [hex, setHex] = useState('');
+  const [uuid, setUuid] = useState('');
 
   function handleHexChange(newHex: string) {
     setHex(newHex);
+    setUuid(hexToUuid(newHex.substring(0, 32)));
+  }
+
+  function handleUuidChange(newUuid: string) {
+    setUuid(newUuid);
+    setHex(uuidToHex(newUuid));
+  }
+
+  function generateRandomUuid() {
+    const newUuid = uuidv4();
+    setUuid(newUuid);
+    setHex(uuidToHex(newUuid));
   }
 
   return (
-    
     <div className="App">
-     <header className="App-header">
+      <header className="App-header">
+      <HexToUuidInput hex={hex} uuid={uuid} onHexChange={handleHexChange} onUuidChange={handleUuidChange} />  
+      <HexToUuidConverter hex={hex} uuid={uuid} onGenerateUuid={generateRandomUuid} />
         
-        <HexToUuidInput onHexChange={handleHexChange} />
-        {hex && <HexToUuidConverter hex={hex} />}
       </header>
-      <div className="infobox"><h1>What's this?</h1>  
-      <p>Copy a UUID from the database (typically RAW(16) from Oracle) and paste it here to get the uuid in the proper format.A UUID conforming to RFC-4122 will be slightly blue. This site is not collecting any data. Nothing leaves your browser.</p></div>
+      <div className="infobox">
+        <h1>What's this?</h1>
+        <p>Copy a UUID from the database (typically RAW(16) from Oracle) and paste it here to get the UUID in the proper format. A UUID conforming to RFC-4122 will be slightly blue. This site is not collecting any data. Nothing leaves your browser.</p>
+      </div>
     </div>
   );
 }
--- src/HexToUuidInput.tsx
+++ src/HexToUuidInput.tsx
@@ -1,52 +1,128 @@
 import React, { useState } from 'react';
 import HexToUuidDisplay from './HexToUuidDisplay';
+import { v4 as uuidv4 } from 'uuid'; // Make sure to install uuid package
+import { ReactComponent as MyIcon } from './copy-icon.svg';
+export function hexToUuid(hex: string): string {
+  const group1 = hex.substring(0, 8);
+  const group2 = hex.substring(8, 12);
+  const group3 = hex.substring(12, 16);
+  const group4 = hex.substring(16, 20);
+  const group5 = hex.substring(20);
 
-function hexToUuid(hex: string): string {
-    const group1 = hex.substring(0, 8);
-    const group2 = hex.substring(8, 12);
-    const group3 = hex.substring(12, 16);
-    const group4 = hex.substring(16, 20);
-    const group5 = hex.substring(20);
-  
-    const uuid = `${group1}-${group2}-${group3}-${group4}-${group5}`;
-    return uuid;
-  }
+  const uuid = `${group1}-${group2}-${group3}-${group4}-${group5}`;
+  return uuid;
+}
+
+export function uuidToHex(uuid: string): string {
+  return uuid.replace(/-/g, '');
+}
 
 interface HexToUuidInputProps {
   onHexChange: (hex: string) => void;
+  onUuidChange: (uuid: string) => void;
+  hex: string;
+  uuid: string;
 }
 
 export function HexToUuidInput(props: HexToUuidInputProps) {
-  const [hex, setHex] = useState('');
-
   function handleHexChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newHex = event.target.value;
-    setHex(newHex);
     props.onHexChange(newHex);
   }
 
+  function handleUuidChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newUuid = event.target.value;
+    props.onUuidChange(newUuid);
+  }
+
   return (
-    <div className="heading"><p>
-	Do not paste sensitive uuids into random websites you don't trust.</p>
+    <div>
+      <p>Do not paste sensitive UUIDs into random websites you don't trust.</p>
       <label htmlFor="hex-input">Hex value from your favourite UUID provider</label>
-      <input id="hex-input" type="text" value={hex} onChange={handleHexChange} />
-    </div> 
+      <input id="hex-input" type="text" value={props.hex} onChange={handleHexChange}     style={{ 
+          width: '80%', 
+          padding: '10px', 
+          margin: '10px 0', 
+          fontSize: '2em' 
+        }} 
+      /><br/>
+      <label htmlFor="uuid-input">UUID value</label>
+      <input id="uuid-input" type="text" value={props.uuid} onChange={handleUuidChange}     style={{ 
+          
+          width: '80%', 
+          padding: '10px', 
+          margin: '10px 0', 
+          fontSize: '2em' 
+        }} 
+      />
+    </div>
   );
 }
 
 interface HexToUuidConverterProps {
   hex: string;
+  uuid: string;
+  onGenerateUuid: () => void;
 }
 
 export function HexToUuidConverter(props: HexToUuidConverterProps) {
-  const strippedHex = props.hex.replace(/-/g, '');
+  const [copySuccess, setCopySuccess] = useState(false);
 
-  const uuid = hexToUuid(strippedHex.substring(0, 32));
+  function handleCopyUuidClick() {
+    navigator.clipboard.writeText(props.uuid)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 2000);
+      })
+      .catch(() => {
+        setCopySuccess(false);
+      });
+  }
 
-  const uuidString = uuid.toString();
+  function handleCopyHexClick() {
+    navigator.clipboard.writeText(props.hex)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 2000);
+      })
+      .catch(() => {
+        setCopySuccess(false);
+      });
+  }
+
   return (
-    <HexToUuidDisplay uuid={uuidString} />
+    <div>
+      <HexToUuidDisplay uuid={props.uuid} /> 
+      <div>
+        UUID: {props.uuid}
+        <button onClick={handleCopyUuidClick}>
+          <MyIcon className="icon" />
+        </button>
+      </div>
+      <div>
+        Hex: {props.hex}
+        <button onClick={handleCopyHexClick}>
+          <MyIcon className="icon" />
+        </button>
+      </div>
+      {copySuccess && <span className="copied">Copied!</span>}
+      <button onClick={props.onGenerateUuid} style={{ 
+          padding: '10px 20px', 
+          margin: '10px 0', 
+          backgroundColor: '#007bff', 
+          color: 'white', 
+          border: 'none', 
+          borderRadius: '5px', 
+          cursor: 'pointer' 
+        }}
+      > Generate Random UUID </button>
+      
+    </div>
   );
 }
 
-export default HexToUuidConverter;
+ 
